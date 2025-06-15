07e5793a0fccbbbdc000929af02e90d9f6d9153b
Kisor-S <146700468+Kisor-S@users.noreply.github.com>
2025-06-15 18:00:09 +0000

    create appspec file

---
+++ scripts/stop_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "===== Script content:"
+cat "$0"
+set -e
+
+# Get container IDs (skipping the header)
+containerid=$(docker ps | awk 'NR>1 {print $1}')
+
+# Remove containers if found
+if [ -n "$containerid" ]; then
+  docker rm -f $containerid
+  echo "Stopped and removed: $containerid"
+else
+  echo "No running containers."
+fi
